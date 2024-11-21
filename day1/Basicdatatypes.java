52680fb18e244be7ebe445d3f758c55fe50a42c8
mahen-saisho <70901793+mahen-saisho@users.noreply.github.com>
2024-11-21 10:43:01 +0530

    Add files via upload

---
+++ day1/Basicdatatypes.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class Basicdatatypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float browserVersion = 130.06F; // float value should have f or F
+		String broswerName = "firefox";
+		boolean isVisible = true;  //boolean default value always false
+		int releaseYear = 1998;
+		char browserLogo = 'f';
+		
+		System.out.println("Browser Version"+ browserVersion);
+		System.out.println("Broswer Name:" + broswerName);
+		System.out.println("Broswer is Open:" + isVisible);
+		System.out.println("Broswer releaser Year:"+releaseYear);
+		System.out.println("Broswer logo:"+browserLogo);
+		
+
+	}
+
+}
